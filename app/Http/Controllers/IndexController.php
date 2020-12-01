b6d1cda7efe6e8ce7cdb8dfc85d66c306c0c0d72
dedimaulana <dedymandiri992@gmail.com>
2020-12-01 21:17:44 +0700

    update tampilan

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,14 +14,15 @@ class IndexController extends Controller
 
     public function store(Request $request)
     {
-        // Newsletter::delete('SUBSCRIBER_EMAIL');
+       // Newsletter::delete('SUBSCRIBER_EMAIL');
 
         if ( ! Newsletter::isSubscribed($request->user_email) )
         {
             Newsletter::subscribePending($request->user_email);
-            return redirect('index')->with('status','thanks for subscription!');
+
+            return redirect('index')->with('status','Thanks for subscription!');
 
         }
-        return redirect('index')->with('status','you are already subscribed!');
+        return redirect('index')->with('status','You are already subscribed!');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,26 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+</head>
+<body style="margin-left:100px;"><br>
+<h3>Integrate Mailchimp Newsletter </h3>
 @if (session('status'))
-    <div class="alert alert-success">
-    {{ session('status') }}
+    <div style="width:1000px;" class="alert alert-success" role="alert">
+        {{ session ('status') }}
     </div>
 @endif
-
+<br>
 <form action="/index/newsletter" method="post">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label><br>
-    <input style="width:300px;height:35px;" type="email" name="user_email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"><br>
-     </div><br>
+    <input style="width:300px;height:45px;" type="email" name="user_email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"><br>
+     </div>
   {{ csrf_field() }}
-    <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</body>
+</html>
 
